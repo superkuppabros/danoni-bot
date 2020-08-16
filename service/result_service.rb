82371beec85235a9b36778253c1d79a465437b02
SKB <superkuppabros1995@gmail.com>
2020-08-16 16:54:31 +0900

    change: MFV2ソース対応

--- service/result_service.rb
+++ service/result_service.rb
@@ -3,10 +3,19 @@ class ResultService
   # @param line [String]
   def analyse(line)
     items = line.split('/')
-    score = items[3].split(':')[1]
-    note_judges = items[5].split('-').map(&:to_i)
-    freeze_judges = items[6].split('-').map(&:to_i)
-    combo_judges = items[7].split('-').map(&:to_i)
+
+    # MFV2's source
+    if items[4].split('-').length == 5
+      score = items[7].delete('Sco').split('-')[1].to_i
+      note_judges = items[4].split('-').map(&:to_i)
+      freeze_judges = items[5].split('-').map(&:to_i)
+      combo_judges = items[6].delete('Mc').split('-').map(&:to_i)
+    else
+      score = items[3].split(':')[1]
+      note_judges = items[5].split('-').map(&:to_i)
+      freeze_judges = items[6].split('-').map(&:to_i)
+      combo_judges = items[7].split('-').map(&:to_i)
+    end
 
     result_data = {
       'ii' => note_judges[0],
@@ -21,14 +30,16 @@ class ResultService
       'score' => score
     }
 
-    recover_num = result_data['ii'] + result_data['syakin'] + result_data['kita']
+    recover_notes_num = result_data['ii'] + result_data['syakin']
+    recover_freezes_num = result_data['kita']
+    recover_num = recover_notes_num + recover_freezes_num
     matari_num = result_data['matari']
     miss_num = result_data['shobon'] + result_data['uwan'] + result_data['ikunai']
-    accuracy = recover_num.fdiv(recover_num + matari_num + miss_num).round(4) * 100
+    accuracy = (recover_num.fdiv(recover_num + matari_num + miss_num) * 100).round(2)
     combo = result_data['maxcombo'] + result_data['frzcombo']
 
-    result_message = "素点: #{recover_num}
-ミス数: #{miss_num}, 達成率: #{accuracy}%, コンボ数: #{combo}"
+    result_message = "素点: #{recover_num} (#{recover_notes_num}+#{recover_freezes_num})
+マターリ数: #{matari_num} ミス数: #{miss_num}, 達成率: #{accuracy}%, コンボ数: #{combo}"
 
     result_message
   end
